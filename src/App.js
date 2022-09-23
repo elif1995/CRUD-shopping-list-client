4249dff8228d8a6ff913cded6ac7cb9fb64e54e1
edimmfs <e21211995@gmail.com>
2022-09-23 18:36:20 +0300

    added some basic style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Axios from 'axios';
 import './App.css';
 import { DeleteOutlined, EditOutlined , PlayCircleOutlined, PlusCircleOutlined} from '@ant-design/icons';
-import { Button, Space } from 'antd';
+import { Button, Space, Card } from 'antd';
 
 function App() {
-
+  const lastFood = useRef(null);
   const [addFood, setAddFood] = useState(false);
   const [foodName, setFoodName] = useState("");
   const [numberToBuy, setNumberToBuy] = useState(0);
@@ -15,11 +15,15 @@ function App() {
   useEffect(() =>{
     Axios.get('https://shopping-list-frenkin.herokuapp.com/read').then((response) => {
       setFoodList(response.data)
+      
     })
+    
   },[foodList])
 
   const addToList = () => {
     Axios.post('https://shopping-list-frenkin.herokuapp.com/insert', {foodName: foodName, numberToBuy:numberToBuy})
+    setAddFood(!addFood);
+    
   }
 
 
@@ -43,20 +47,21 @@ function App() {
       <input type="text" onChange={(e) => {setFoodName(e.target.value);}}/>
       <lable>How much to buy</lable>
       <input type="number" onChange={(e) => {setNumberToBuy(e.target.value);}}/>
-      <button style={{ background:'transparent', boxShadow: ' 4px 4px 15px lightgrey', border:'none', cursor: 'pointer'}} onClick={addToList}><PlusCircleOutlined style={{color: 'green'}} /></button></div>}
+      <button style={{ background:'transparent', boxShadow: ' 4px 4px 15px lightgrey', border:'none', cursor: 'pointer'}} onClick={addToList }><PlusCircleOutlined style={{color: 'green'}} /></button></div>}
       
       <br/>
       <h1 style={{textDecoration: 'underline'}}>Food to Buy:</h1>
       <div className="app-items">
       {foodList.map(({foodName,howMuchToBuy,_id}) => 
         (<div className="items"> 
-          <h1>buy <span style={{color: 'green', border: '1px dotted green', padding: '5px'}}>{howMuchToBuy}</span> <span style={{color: 'green', border: '1px dotted green', padding: '5px'}}>{foodName} </span></h1>
+          <p>name: <span style={{color: 'green', border: '1px dotted green', padding: '5px',marginRight: '5px'}}> {foodName} </span> quantity: <span style={{color: 'green', border: '1px dotted green', padding: '5px'}}>{howMuchToBuy}</span> </p>
           
           <input type="text" 
-                  placeholder="Enter Food Count..." 
+                  placeholder="Change Food Count..." 
                     onChange={(e) => {setNewFoodNumber(e.target.value);}}/>
-          <button onClick={() => {updateFood(_id)}}><EditOutlined/></button>
-          <button onClick={() => {deleteFood(_id)}}><DeleteOutlined/></button>
+          <button style={{backgroundColor: 'lightyellow', border: '1px solid lightgrey'}} onClick={() => {updateFood(_id)}}><EditOutlined/></button>
+          <button style={{backgroundColor: 'pink', border: '1px solid lightgrey'}} onClick={() => {deleteFood(_id)}}><DeleteOutlined/></button>
+          <div ref={lastFood}></div>
         </div>)
       )}
       </div>
